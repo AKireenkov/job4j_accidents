25242e6bdf28d77413fb5a6091bf7bbbaac6bcf0
andreykireenkov <kireeshka32@gmail.com>
2023-06-10 22:09:55 +0300

    1. Загрузка данных в вид [#295782 # [#295782]]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -2,16 +2,17 @@ package ru.job4j.accidents.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/accidents")
 public class IndexController {
 
-    @GetMapping
-    public String getAllAccidents() {
+    @GetMapping("/")
+    public String getAllAccidents(Model model) {
+        model.addAttribute("user", "Andrey Kireenkov");
         return "index";
     }
 }
