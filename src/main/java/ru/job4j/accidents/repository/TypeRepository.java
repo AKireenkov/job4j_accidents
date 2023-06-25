712100e440335115089a07484e6b9ee31c07035e
andreykireenkov <kireeshka32@gmail.com>
2023-06-25 14:44:06 +0300

    2. Spring Data [#296073 # [#296073 #425068]]

--- src/main/java/ru/job4j/accidents/repository/TypeRepository.java
+++ src/main/java/ru/job4j/accidents/repository/TypeRepository.java
@@ -3,15 +3,9 @@ package ru.job4j.accidents.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.accidents.model.AccidentType;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public interface TypeRepository extends CrudRepository<AccidentType, Integer> {
 
-    default Collection<AccidentType> findAllTypes() {
-        List<AccidentType> types = new ArrayList<>();
-        findAll().forEach(types::add);
-        return types;
-    }
+    List<AccidentType> findAll();
 }
