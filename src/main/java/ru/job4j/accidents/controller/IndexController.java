1a64db8f930a368a80bef16b44edcc317073b85c
AKireenkov <Kireenkov@alfabank.ru>
2023-06-29 00:03:33 +0300

    1. Авторизация через JDBC [#2094  [#2094]]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,6 +1,7 @@
 package ru.job4j.accidents.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +14,8 @@ public class IndexController {
     private AccidentService accidentService;
 
     @GetMapping("/")
-    public String getAllAccidents(Model model) {
-        model.addAttribute("user", "Andrey Kireenkov");
+    public String index(Model model) {
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         model.addAttribute("accidents", accidentService.findAll());
         return "index";
     }
